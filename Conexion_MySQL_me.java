5a037c22f7abcd2afb02317b2bd2def83075bd24
Julio C. Ramos <ramos.isw@gmail.com>
2013-08-24 15:29:09 -0700

    Update Conexion_MySQL_me.java

--- Conexion_MySQL_me.java
+++ Conexion_MySQL_me.java
@@ -22,7 +22,8 @@ public class Conexion {
 
         try {
             //Aqui se carga el Driver de MySQL
-            Class.forName("org.gjt.mm.mysql.Driver");
+            //Class.forName("org.gjt.mm.mysql.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             //Aqui se crea el enlace hacia la base de datos mediante la variable link
             link = DriverManager.getConnection(this.url, this.user, this.pass);
 
