5c7c34c398677513208db20111bf8aa9c572cc6f
Julio C. Ramos <ramos.isw@gmail.com>
2013-08-24 17:07:52 -0700

    Update ConexionMySQL.java

--- ConexionMySQL.java
+++ ConexionMySQL.java
@@ -14,6 +14,16 @@ public class ConexionMySQL {
 			Class.forName("com.mysql.jdbc.Driver");
 			// Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
 			url = "jdbc:mysql://" + servidor + "/" + database;
+			/*
+			*si mas adelante en proyectos posteriores necesitan conectarse a una base de datos que no
+			*sea local ocuparan colocar un puerto, que sera proporcionado por el administrador
+			*pra darles acceso total
+			**entonces el url quedaria de la sig. manera
+			* url = "jdbc:mysql://" + servidor + ":port/" + database;
+			* remplasan la palabra "port" por el numero de puerto para la conexion. MySQL generalmente
+			*es 3306
+			*
+			*/
 			conexion = DriverManager.getConnection(url, usuario, password);
 			System.out.println("Conexion a Base de Datos " + url
 					+ " . . . . .Ok");
